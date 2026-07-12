0b62ed9ed2c2a9208e5a10afe1d72cbe13fd3f04
Oguz Agac <oguzkaanagac@pm.me>
2026-07-12 12:58:43 +0200

    tab accept

--- init.lua
+++ init.lua
@@ -677,6 +677,7 @@ do
   local servers = {
      clangd = {},
      gopls = {},
+     zls = {},
     -- pyright = {},
      rust_analyzer = {},
     --
@@ -835,7 +836,7 @@ do
       -- <c-k>: Toggle signature help
       --
       -- See `:help blink-cmp-config-keymap` for defining your own keymap
-      preset = 'default',
+      preset = 'super-tab',
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
